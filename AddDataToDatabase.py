1631b3310a5cdd30b24fd6ab64eec1953489097b
OMKAR MUDUGADE <114016082+OmkarMudugade@users.noreply.github.com>
2023-08-24 18:01:31 +0530

    Add files via upload

---
+++ AddDataToDatabase.py
@@ -0,0 +1,117 @@
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendancerealtime-14f52-default-rtdb.firebaseio.com"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "7777":
+        {"name": "saurabh bhai",
+         "major": "AI",
+         "starting_year": 2017,
+         "total_attendance": 0,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "0000":
+        {"name": "aayush",
+         "major": "AI",
+         "starting_year": 2017,
+         "total_attendance": 0,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "5555":
+        {"name": "VISHVAJEET",
+         "major": "AI",
+         "starting_year": 2017,
+         "total_attendance": 0,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "1111":
+        {"name":"shivani",
+         "major":"AI",
+         "starting_year": 2017,
+         "total_attendance": 7,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "1022":
+        {"name":"Akanksha Mali",
+         "major":"AI",
+         "starting_year": 2017,
+         "total_attendance": 7,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "1122":
+        {"name":"Neha",
+         "major":"AI",
+         "starting_year": 2017,
+         "total_attendance": 7,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "1121":
+        {"name":"OMKAR",
+         "major":"AI",
+         "starting_year": 2017,
+         "total_attendance": 0,
+         "standing": "G",
+         "year": 4,
+         "last_attendance_time": "2022-12-11 00:54:34"
+
+         },
+    "321654":
+        {
+            "name": "MURTUZA ",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
